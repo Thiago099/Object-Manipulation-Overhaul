8bcbbf607af31ef3c4d16de74b0308b11577f5be
Thiago009 <thiago.kaique01@gmail.com>
2024-07-07 13:21:46 -0300

    Removed the menu framework as it is not been used

--- include/Application/UI.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Lib/SKSEMenuFramework.h"
-#include "Application/ObjectManipulationManager.h"
-namespace UI {
-    void Register();
-    namespace Menu1 {
-        void __stdcall Render();
-    }
-};
